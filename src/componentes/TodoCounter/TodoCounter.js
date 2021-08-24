422d6f6fa149b140ccc0889a57379bf8b62e4fa7
aaguilarv <aaguilarv@loggro.com>
2021-08-24 13:12:53 -0500

    Se crea estilos css en carpeta TodoCount.css

---
+++ src/componentes/TodoCounter/TodoCounter.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../TodoCounter/TodoCount.css';
+
+
+/*
+    const estilos={
+    color: 'red',
+    backgroundColor: 'yellow'
+};
+*/
+
+function TodoCounter() {
+
+
+
+    return (
+        <h2 className = "TodoCounter">Has completado 2 de 3 TODOS</h2>
+    );
+}
+
+export { TodoCounter };
