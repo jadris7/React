9fcf89a9cdba7ca6aa30ec2918c01ffc87d5fe8f
aaguilarv <aaguilarv@loggro.com>
2021-08-24 17:16:20 -0500

    Eventos

--- src/componentes/Todo_Search/TodoSearch.js
+++ src/componentes/Todo_Search/TodoSearch.js
@@ -2,8 +2,17 @@ import React from 'react'
 import '../Todo_Search/TodoSearch.css'
 
 function TodoSearch() {
+
+    const onSearchValueChange = (event) => {
+        console.log(event.target.value);
+      };
+
     return (     
-        <input className="TodoSearch"placeholder="Search..."/>
+        <input
+            className="TodoSearch"
+            placeholder="Search..."
+            onChange={onSearchValueChange}
+        />
     );
 }
 
