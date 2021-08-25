0830dc14ed691213f3c245d38203fbe7a7da9f5d
aaguilarv <aaguilarv@loggro.com>
2021-08-25 08:23:43 -0500

    Revert "salto de línea en alerta de eliminación"
    This reverts commit 76730634be7ed080314566cedc3e4be307c812ec.

--- src/componentes/TodoItem/TodoItem.js
+++ src/componentes/TodoItem/TodoItem.js
@@ -4,10 +4,10 @@ import '../TodoItem/TodoItem.css';
 function TodoItem(props) {
 
     const onComplete = () => {
-        alert('Ya completaste la tarea \n' + props.text);
+        alert('Ya completaste la tarea' + props.text);
       };
       const onDelete = () => {
-        alert('Borraste la tarea: \n' + props.text);
+        alert('Borraste la tarea ' + props.text);
       };
 
     return (
